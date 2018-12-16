d3858619f0762756db9230f0df48460709530e8b
Elijah Prince <elijahprince73@gmail.com>
2018-12-16 15:46:42 -0800

    Added firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBQSttVuiCUcs5Kyrkop8QRdVl5YqyFzGE',
+            authDomain: 'react-auth-93261.firebaseapp.com',
+            databaseURL: 'https://react-auth-93261.firebaseio.com',
+            projectId: 'react-auth-93261',
+            storageBucket: 'react-auth-93261.appspot.com',
+            messagingSenderId: '882084102890'
+        });
+    }
+
     render() {
         return (
             <View>
-                <Header headerText="Authentication" />
-                <Text>App</Text>
+                <Header headerText='Authentication' />
+                <LoginForm />
             </View>
         );
     }
